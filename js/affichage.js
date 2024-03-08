65ac11d503b6b95958a77a96ce1176b8050d7d78
ArthurDenis62 <arthurdenis.pro@gmail.com>
2024-03-08 03:13:06 +0100

    Affichage up

--- js/affichage.js
+++ js/affichage.js
@@ -12,18 +12,39 @@ function displayRecipes () {
     imgElt.classList.add('c_img')
     recetteDiv.appendChild(imgElt)
 
+    const contentDiv = document.createElement('div')
+    contentDiv.classList.add('c_content')
+    recetteDiv.appendChild(contentDiv)
+
     const nomElt = document.createElement('h3')
     nomElt.textContent = r.name
-    recetteDiv.appendChild(nomElt)
+    contentDiv.appendChild(nomElt)
+
+    const titleRecipe = document.createElement('span')
+    titleRecipe.textContent = 'RECETTE'
+    contentDiv.appendChild(titleRecipe)
 
     const descElt = document.createElement('p')
     descElt.textContent = r.description
-    recetteDiv.appendChild(descElt)
+    contentDiv.appendChild(descElt)
+
+    const ingreDiv = document.createElement('div')
+    ingreDiv.classList.add('c_ingredients')
+    recetteDiv.appendChild(ingreDiv)
+
+    const titleIngre = document.createElement('span')
+    titleIngre.textContent = 'INGRÉDIENTS'
+    ingreDiv.appendChild(titleIngre)
 
     r.ingredients.forEach(ingredient => {
-      const ingrElt = document.createElement('span')
-      ingrElt.textContent = `${ingredient.quantity || ''} ${ingredient.unit || ''} ${ingredient.ingredient}`
-      recetteDiv.appendChild(ingrElt)
+      const ingrDiv = document.createElement('div')
+      ingreDiv.appendChild(ingrDiv)
+      const ingrElt1 = document.createElement('span')
+      const ingrElt2 = document.createElement('span')
+      ingrElt1.textContent = `${ingredient.ingredient}`
+      ingrElt2.textContent = `${ingredient.quantity || ''} ${ingredient.unit || ''}`
+      ingrDiv.appendChild(ingrElt1)
+      ingrDiv.appendChild(ingrElt2)
     })
 
     containerRecipes.appendChild(recetteDiv)
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Afficher toutes les recettes grâce au json
-// Afficher les options des filtres dans les select
+// Afficher les options des filtres dans les select (Utiliser le proxy dans filterSelect.js)
 // Pouvoir rechercher dans les filtres (Commencer la racherche par appliance puis ustenils et ingrédients)
 // Une fois le tag choisis afficher les résultats voulus, pouvoir le supprimer
 // Revoir les select une fois un tag choisi en fonction des correspondances
 // Recherche principal, faire les 2 méthodes
 // Actualiser le total de recette
 // Utiliser le proxy pour rendre les éléments
+
+// Plutot utiliser des composants réutilisables dans affichage.js
 
 import displayRecipes from './affichage.js'
 import { fillSelectOptionsWithFilter, ingredientsList, appliancesList, utensilsList } from './filterSelect.js'
